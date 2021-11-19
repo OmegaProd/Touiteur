842b54ee0c110e6f265a768758c1219a6fa9d0ea
Téo Gadea <the.destructor1812@gmail.com>
2021-11-19 09:10:20 +0100

    Creation blogcontroller

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'blog')]
-    public function index(): Response
+    /**
+     * @Route("/blog", name="blog")
+     */
+
+    public function index()
     {
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
