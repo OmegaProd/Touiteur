7e21d3ec2a09d08c35868ca86d1b3d189f5baa90
Aled <sam31.devaux@gmail.com>
2021-12-06 16:21:15 +0100

    login.php

---
+++ model/login.php
@@ -0,0 +1,33 @@
+<?php 
+
+session_start();
+
+function login($username,$password,$bdd){
+
+    $reqValidation = $bdd->prepare('SELECT * FROM profiles WHERE username = :pseudo'); //recuperation du champ correspondant au pseudo
+    $reqValidation->execute(array(
+	':pseudo' => htmlspecialchars($username)
+    ));
+
+
+    $result = $reqValidation->fetch(PDO::FETCH_ASSOC); //Conversion du résultat en array
+    
+
+    if (password_verify($password,$result['password'])) //Vérification du hash
+    {
+    $_SESSION['id'] = $result['id'];
+    $_SESSION['pseudo'] = $result['username'];
+    $_SESSION['email'] = $result['email'];
+    $_SESSION['password'] = $result['password'];
+
+    
+   
+    header('location:./Shop.php');
+    } 
+
+    else 
+    {
+    echo "Pseudo ou mot de passe incorrect";
+    header('location:./Error/Errorlogin.php');
+    }
+}
